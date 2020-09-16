99faca67457990182fc8c460ac4246a9affa0b4a
Akash Ravichandran <akash.chandran30@gmail.com>
2020-09-16 15:02:38 +0530

    inbuilt search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,31 @@
 (() => {
   // Theme switch
-  const root = document.body;
-  const themeSwitch = document.getElementById("mood");
-  const themeData = root.getAttribute("data-theme");
+  const body = document.body;
+  const lamp = document.getElementById("mood");
 
-  if (themeSwitch) {
-    initTheme(localStorage.getItem("theme"));
-    themeSwitch.addEventListener("click", () =>
-      toggleTheme(localStorage.getItem("theme"))
-    );
-
-    function toggleTheme(state) {
-      var utterances = document.querySelector('iframe');
-      if (state === "dark") {
-        localStorage.setItem("theme", "light");
-        root.removeAttribute("data-theme");
-        utterances.contentWindow.postMessage(
-          {type: 'set-theme',theme: 'github-light'},
-          'https://utteranc.es'
-        );
-      } else if (state === "light") {
-        localStorage.setItem("theme", "dark");
-        document.body.setAttribute("data-theme", "dark");
-        utterances.contentWindow.postMessage(
-          {type: 'set-theme',theme: 'github-dark'},
-          'https://utteranc.es'
-        );
-      } else {
-        initTheme(state);
-      }
+  const toggleTheme = (state) => {
+    if (state === "dark") {
+      localStorage.setItem("theme", "light");
+      body.removeAttribute("data-theme");
+    } else if (state === "light") {
+      localStorage.setItem("theme", "dark");
+      body.setAttribute("data-theme", "dark");
+    } else {
+      initTheme(state);
     }
+  };
 
-    function initTheme(state) {
-      if (state === "dark") {
-        document.body.setAttribute("data-theme", "dark");
-      } else if (state === "light") {
-        root.removeAttribute("data-theme");
-      } else {
-        localStorage.setItem("theme", themeData);
-      }
-    }
-  }
+  lamp.addEventListener("click", () =>
+    toggleTheme(localStorage.getItem("theme"))
+  );
+
+  // Blur the content when the menu is open
+  const cbox = document.getElementById("menu-trigger");
+
+  cbox.addEventListener("change", function () {
+    const area = document.querySelector(".wrapper");
+    this.checked
+      ? area.classList.add("blurry")
+      : area.classList.remove("blurry");
+  });
 })();
